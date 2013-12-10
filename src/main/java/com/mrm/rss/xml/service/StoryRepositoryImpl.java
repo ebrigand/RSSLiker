84b5f89103eab6fc96df33eb07498756450f84b3
ebrigand <ebrigand@ippon.fr>
2013-12-10 00:37:39 +0000

    Fix bug in find

--- src/main/java/com/mrm/rss/xml/service/StoryRepositoryImpl.java
+++ src/main/java/com/mrm/rss/xml/service/StoryRepositoryImpl.java
@@ -85,7 +85,7 @@ public class StoryRepositoryImpl implements StoryRepository<Story> {
     Repo repo = xmlRepoManager.openRepo();
     for (int i = 0; i < repo.getStories().size(); i++) {
       Story story = repo.getStories().get(i);
-      if (story.getUriWithoutSpecialChars().equals(uriWithoutSpecialChars)) {
+      if (story.getUriWithoutSpecialChars().equals(story)) {
         return story;
       }
     }
