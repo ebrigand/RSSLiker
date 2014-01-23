a534f13bc992b9226d2df63d0487b6616d37d21d
ebrigand <ebrigand@ippon.fr>
2014-01-23 14:21:29 +0000

    Loop with an iterator on syndentries

--- src/main/java/com/mrm/rss/service/async/LikeCountsUpdateScheduler.java
+++ src/main/java/com/mrm/rss/service/async/LikeCountsUpdateScheduler.java
@@ -2,6 +2,7 @@ package com.mrm.rss.service.async;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -64,7 +65,8 @@ public class LikeCountsUpdateScheduler {
   private List<Story> getMatchingStoriesWithCurrentReadStories(Set<Story> repoStories, Set<SyndEntry> syndEntries) throws RSSLikerServiceException {
     List<Story> matchingStories = new ArrayList<Story>();
     for (Story story : repoStories) {
-      for (SyndEntry syndEntry : syndEntries) {
+      for (Iterator<SyndEntry> it = syndEntries.iterator(); it.hasNext();) {
+        SyndEntry syndEntry = it.next();
         if (story.getUriWithoutSpecialChars().equals(URIUtil.getUriWithoutSpecialChars(syndEntry.getUri()))) {
           matchingStories.add(story);
           break;
